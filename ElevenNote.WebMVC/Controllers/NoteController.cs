762852e30f170b44c5590abb7d9872472ef44791
Dustin Cummings <dustincummings15@gmail.com>
2018-11-21 15:12:32 -0500

    End of Part 5

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -21,5 +21,16 @@ namespace ElevenNote.WebMVC.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]  
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
